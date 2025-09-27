7a4fd1cbab40354e7c2f00f891b48eb679a615ac
Ben Swiger <bencswiger01@gmail.com>
2025-09-27 16:06:03 -0500

    My First Commit 2025-09-27

---
+++ Assets/BenSwiger.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class BenSwiger
+{
+	// I shall be telling this with a sigh,
+	// somewhere ages and ages hence:
+	// Two roads diverged in a wood, and I,
+	// I took the one less traveled by,
+	// and that has made all the difference.
+}
